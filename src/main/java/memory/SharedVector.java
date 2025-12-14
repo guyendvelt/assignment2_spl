1e8d4b590e92a93ed8ac9b35db0944bcec4ee7be
guy <guyendvelt3010@gmail.com>
2025-12-14 22:15:37 +0200

    implementing shared vector

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -17,61 +17,181 @@ public class SharedVector {
     public double get(int index) {
         // TODO: return element at index (read-locked)
         readLock();
-        try{
+        try {
             return vector[index];
-        }finally {
+        } finally {
             readUnlock();
         }
     }
 
     public int length() {
-       readLock();
-       try{
-           return vector.length;
-       }finally {
-           readUnlock();
-       }
+        readLock();
+        try {
+            return vector.length;
+        } finally {
+            readUnlock();
+        }
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
+        readLock();
+        try {
+            return orientation;
+        } finally {
+            readUnlock();
+        }
     }
 
     public void writeLock() {
         // TODO: acquire write lock
+        this.lock.writeLock().lock();
+
     }
 
     public void writeUnlock() {
         // TODO: release write lock
+        this.lock.writeLock().unlock();
     }
 
     public void readLock() {
         // TODO: acquire read lock
+        this.lock.readLock().lock();
     }
 
     public void readUnlock() {
         // TODO: release read lock
+        this.lock.readLock().unlock();
     }
 
     public void transpose() {
         // TODO: transpose vector
+        writeLock();
+        try {
+            VectorOrientation curr = this.orientation;
+            if (curr == VectorOrientation.ROW_MAJOR) {
+                this.orientation = VectorOrientation.COLUMN_MAJOR;
+            } else {
+                this.orientation = VectorOrientation.ROW_MAJOR;
+            }
+        } finally {
+            writeUnlock();
+        }
     }
 
     public void add(SharedVector other) {
         // TODO: add two vectors
+        this.writeLock();
+        other.readLock();
+        try {
+            if (this.vector.length != other.vector.length) {
+                throw new IllegalArgumentException("Vectors' lengths does not match");
+            } else if (this.orientation != other.orientation) {
+                throw new IllegalArgumentException("Orientations does not match");
+            }
+            for (int i = 0; i < this.vector.length; i++) {
+                this.vector[i] += other.vector[i];
+            }
+        } finally {
+            other.readUnlock();
+            this.writeUnlock();
+        }
     }
 
     public void negate() {
         // TODO: negate vector
+        this.writeLock();
+        try {
+            for (int i = 0; i < vector.length; i++) {
+                this.vector[i] = 0 - this.vector[i];
+            }
+        } finally {
+            this.writeUnlock();
+        }
     }
 
     public double dot(SharedVector other) {
         // TODO: compute dot product (row · column)
-        return 0;
+        this.readLock();
+        other.readLock();
+        double dotRes = 0;
+        try {
+            if (this.vector.length != other.vector.length) {
+                throw new IllegalArgumentException("Vectors' lengths does not match");
+            } else if (this.orientation == other.orientation) {
+                throw new IllegalArgumentException("Orientations does not match");
+            }
+            for (int i = 0; i < this.vector.length; i++) {
+                dotRes += this.vector[i] * other.vector[i];
+            }
+            return dotRes;
+        } finally {
+            other.readUnlock();
+            this.readUnlock();
+        }
+
     }
 
     public void vecMatMul(SharedMatrix matrix) {
         // TODO: compute row-vector × matrix
+        if (matrix == null) {
+            throw new IllegalArgumentException("matrix cannot be null");
+        }
+        double[] result;
+        SharedVector tempVector;
+        if (matrix.getOrientation() == VectorOrientation.COLUMN_MAJOR) {
+            if (this.length() != matrix.get(0).length()) {
+                throw new IllegalArgumentException("Dimensions mismatch");
+            }
+            result = new double[matrix.length()];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = this.dot(matrix.get(i));
+            }
+            tempVector = new SharedVector(result, VectorOrientation.ROW_MAJOR);
+
+//        } else {
+//            if (this.length() != matrix.length()) {
+//                throw new IllegalArgumentException("Dimensions mismatch");
+//            }
+//            double[] myVectorData = new double[this.vector.length];
+//            this.readLock();
+//            try {
+//                for (int i = 0; i < this.vector.length; i++) {
+//                    myVectorData[i] = this.vector[i];
+//                }
+//
+//            } finally {
+//                this.readUnlock();
+//            }
+//
+//            int resSize = matrix.get(0).length();
+//            result = new double[resSize];
+//            tempVector = new SharedVector(result, VectorOrientation.ROW_MAJOR);
+//            for (int i = 0; i < this.length(); i++) {
+//                double scalar = myVectorData[i];
+//                SharedVector currRowVec = matrix.get(i);
+//                double[] tempRowArr = new double[result.length];
+//                currRowVec.readLock();
+//                try{
+//                    for (int j = 0; j < tempRowArr.length; j++) {
+//                        tempRowArr[j] = currRowVec.vector[j] * scalar;
+//                    }
+//                }finally {
+//                    currRowVec.readUnlock();
+//                }
+//
+//                SharedVector tempRowVec = new SharedVector(tempRowArr, VectorOrientation.ROW_MAJOR);
+//                tempVector.add(tempRowVec);
+//            }
+//        }
+            writeLock();
+            try {
+                this.vector = tempVector.vector;
+            } finally {
+                writeUnlock();
+            }
+
+        }
     }
+
 }
