ea7b6a2ee4f81559fb9a53ff3ab468992d778e1c
guy <guyendvelt3010@gmail.com>
2025-12-15 16:04:36 +0200

    almost finished implementing tired thread and tired executor

--- src/main/java/scheduling/TiredExecutor.java
+++ src/main/java/scheduling/TiredExecutor.java
@@ -13,26 +13,57 @@ public class TiredExecutor {
 
     public TiredExecutor(int numThreads) {
         // TODO
-        if(numThreads =< 0){
-            throw new IllegalArgumentException("numThreads should be bigger than 0")
+        if(numThreads <= 0){
+            throw new IllegalArgumentException("numThreads should be bigger than 0");
         }
         workers = new TiredThread[numThreads];
-
-    }
-
-    public void submit(Runnable task) {
-        // TODO
-        if(!idleMinHeap.isEmpty()) {
-
+        double factor = 0.5 + Math.random();
+        for (int i = 0; i < numThreads; i++) {
+            workers[i] =  new TiredThread(i, factor);
+            workers[i].start();
+            idleMinHeap.add(workers[i]);
         }
     }
 
-    public void submitAll(Iterable<Runnable> tasks) {
+    public void submit(Runnable task) throws InterruptedException {
+        // TODO
+
+        inFlight.incrementAndGet();
+        TiredThread currThread = idleMinHeap.take();
+        Runnable wrappedTask = () -> {
+            try{
+                task.run();
+            }finally {
+                idleMinHeap.offer(currThread);
+                if(inFlight.decrementAndGet() == 0){
+                    synchronized (this){
+                        this.notifyAll();
+                    }
+                }
+            }
+        };
+        currThread.newTask(wrappedTask);
+    }
+
+    public void submitAll(Iterable<Runnable> tasks) throws InterruptedException {
         // TODO: submit tasks one by one and wait until all finish
+        for(Runnable task : tasks){
+            submit(task);
+        }
+        synchronized (this){
+            while(inFlight.get() > 0){
+                this.wait();
+            }
+        }
     }
 
     public void shutdown() throws InterruptedException {
         // TODO
+        for(TiredThread worker : workers){
+            worker.shutdown();
+        }
+        idleMinHeap.clear();
+
     }
 
     public synchronized String getWorkerReport() {
